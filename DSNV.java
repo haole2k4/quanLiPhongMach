6ecfa5f1b6162e9ab70719ad04e0632da8c505ea
xviethoangdz <144246997+xviethoangdz@users.noreply.github.com>
2023-11-08 13:57:05 +0700

    4 class nhan vien

---
+++ DSNV.java
@@ -0,0 +1,328 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+public class DSNV {
+    private NhanVien[] dsnv = new NhanVien[1];
+    private int soLuongNV;
+    Scanner input = new Scanner(System.in);
+    DSNV()
+    {
+        soLuongNV = 0;
+        dsnv[0] = new NhanVien();
+        dsnv[0] = null;
+    }
+    DSNV(NhanVien [] ds, int sl)
+    {
+        dsnv = ds;
+        soLuongNV = sl;
+    }
+    DSNV(DSNV ds)
+    {
+        dsnv = ds.dsnv;
+        soLuongNV = ds.soLuongNV;
+    }
+    public String fileNV()
+    {
+        return "E:\\java\\baitap\\doan\\NHANVIEN\\NhanVien.txt";
+    }
+//ham lay dong de ghi vao file
+    public String getLine(int i)
+    {
+        return (dsnv[i].getMaNV()+","+dsnv[i].getHo()+","+dsnv[i].getTen()+","+dsnv[i].getGioiTinh()+","+dsnv[i].getNgaySinh()+","+dsnv[i].getChucVu()+","+dsnv[i].getHeSoLuong()+","+dsnv[i].getSDT()+","+dsnv[i].getHoatDong());
+    }
+//ham de xoa data trong 1 file
+    public void xoaDataFile(String filePath) {
+        try {
+            FileWriter writer = new FileWriter(filePath, false);
+            writer.write("");
+
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("loi");
+        }
+    }
+//ham ghi file
+    public void ghiLine(String fileName,String s)
+    {
+        // xoa data trong file tr
+        File outFile = new File(fileName);
+        PrintWriter write=null;
+            try {
+
+                write= new PrintWriter(new FileWriter(outFile,true));
+        
+                }
+        
+               catch (Exception e){ System.out.println("Khong the tao tap tin ket qua");
+        
+                                    return;
+               }
+               write.print(s);
+               write.print("\n");
+               write.close();
+    }
+    void ghiFile()
+    {
+        xoaDataFile(fileNV());
+        ghiLine(fileNV(),String.valueOf(soLuongNV));
+        for (int i=0;i<soLuongNV;i++)
+        {
+            ghiLine(fileNV(),getLine(i));
+        }
+    }
+
+
+
+        void nhap()
+        {
+            //xoa data trong file truoc khi ghi lai file moi
+            xoaDataFile(fileNV());
+            Scanner input = new Scanner(System.in);
+            System.out.print(" - So nhan vien: "); 
+            soLuongNV = input.nextInt();
+            dsnv = new NhanVien[soLuongNV];
+            for (int i=0;i<soLuongNV;i++)
+            {
+                dsnv[i] = new NhanVien();
+                System.out.print("Nhan vien ["+(i+1)+"] :\n");
+                dsnv[i].nhap();
+                System.out.print("\n");
+            }
+            //ghi lai vao file
+            ghiFile();
+        }
+        void docData()
+        {
+            File inFile = new File(fileNV());
+            Scanner read = null;
+            try
+            {
+                read = new Scanner(inFile);
+            }
+            catch (Exception e)
+            {
+                System.out.print("khong the mo file NhanVien.txt");
+                return ;
+            }
+            String sl = read.nextLine();
+            soLuongNV = Integer.parseInt(sl);
+            dsnv = new NhanVien[soLuongNV];
+            for (int i=0;i<soLuongNV;i++)
+            {
+                dsnv[i] = new NhanVien();
+                String line = read.nextLine();
+                String [] str = line.split(",");
+                dsnv[i].setMaNV(str[0]);
+                dsnv[i].setHo(str[1]);
+                dsnv[i].setTen(str[2]);
+                dsnv[i].setGioiTinh(str[3]);
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                LocalDate ngaySinh = LocalDate.parse(str[4], formatter);
+                dsnv[i].setNgaySinh(ngaySinh);
+                dsnv[i].setChucVu(str[5]);
+                dsnv[i].setHeSoLuong(Double.parseDouble(str[6]));
+                dsnv[i].setSDT(str[7]);
+                dsnv[i].setHoatDong(Boolean.parseBoolean(str[8]));
+            }
+            read.close();
+        }
+    void xuat()
+        {
+            docData();
+            System.out.println("Danh sach nhan vien: ");
+            for (int i=0;i<soLuongNV;i++)
+            {
+                dsnv[i].xuat();
+                System.out.print("\n");
+            }
+        }
+
+
+    void them()
+    {
+        dsnv= Arrays.copyOf(dsnv,soLuongNV+1);
+        dsnv[soLuongNV] = new NhanVien();
+        dsnv[soLuongNV].nhap();
+        soLuongNV++;
+        ghiFile();
+    }
+
+    void sua()
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print(" - Nhap ma nhan vien can sua: ");
+        String ma = input.nextLine();
+        boolean flag = false;
+        for (int i=0;i<soLuongNV;i++)
+        {
+            if (ma.equals(dsnv[i].getMaNV()))
+            
+            {
+                flag = true;
+                dsnv[i].sua();
+            }
+        }
+        if (!flag )
+        System.out.println("khong tim thay nhan vien");
+        ghiFile();
+        
+    }
+
+    
+    void sua(String ma)
+    {
+        boolean flag = false;
+        for (int i=0;i<soLuongNV;i++)
+        {
+            if (ma.equals(dsnv[i].getMaNV()))
+            
+            {
+                flag = true;
+                dsnv[i].sua();
+            }
+        }
+        if (!flag )
+        System.out.println("khong tim thay nhan vien");
+
+//sua trong file
+        ghiFile();
+
+
+    }
+    boolean locThongTin(String line,String s)
+    {
+        if (line.indexOf(s) == -1)
+        return false;
+        else
+        return true;
+    }
+
+
+
+
+    void xoa()
+    {
+//xoa trong array hien tai
+        System.out.print(" - Nhap ma nhan vien can xoa: ");
+        Scanner input = new Scanner(System.in);
+        String ma;
+        ma = input.nextLine();
+        boolean flag = false;
+        for (int i=0;i<soLuongNV;i++)
+        {
+            if (ma.equals(dsnv[i].getMaNV()))
+            {
+                flag = true;
+                for (int j=i;j<soLuongNV-1;j++)
+                {
+                    dsnv[j] = dsnv [ j+1];
+                }
+                soLuongNV--;
+            }
+
+        }
+        if (!flag)
+        {
+            System.out.println("Khong tim thay ma nhan vien!");
+            return;
+        }
+        ghiFile();
+    }
+
+
+
+    void xoa(String ma)
+    {
+//xoa trong array hien tai
+
+        boolean flag = false;
+        for (int i=0;i<soLuongNV;i++)
+        {
+            if (ma.equals(dsnv[i].getMaNV()))
+            {
+                flag = true;
+                for (int j=i;j<soLuongNV-1;j++)
+                {
+                    dsnv[j] = dsnv [ j+1];
+                }
+                soLuongNV--;
+            }
+
+        }
+        if (!flag)
+        {
+            System.out.println("Khong tim thay ma nhan vien!");
+            return;
+        }
+// xoa trong file
+        ghiFile();
+    }
+
+    public NhanVien timkiem()
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print(" - Nhap ma nhan vien hoac ten nhan vien can tim kiem: ");
+        String ma = input.nextLine();
+        for (int i=0;i<soLuongNV;i++)
+        {
+            if ((ma.equals(dsnv[i].getMaNV())) || (ma.equals(dsnv[i].getTen())) )
+            {
+                return dsnv[i];
+            }
+        }
+        System.out.println("Khong tim thay nhien vien");
+        NhanVien nv = new NhanVien();
+        return nv;
+    }
+
+    public NhanVien timkiem(String ma)
+    {
+       for (int i=0;i<soLuongNV;i++)
+        {
+            if ((ma.equals(dsnv[i].getMaNV())) || (ma.equals(dsnv[i].getTen())) )
+            {
+                return dsnv[i];
+            }
+        }
+        System.out.println("Khong tim thay nhien vien");
+        NhanVien nv = new NhanVien();
+        return nv;
+    }
+
+    public void thongKeHoatDong()
+    {
+        int hd =0;
+        for (int i=0;i<soLuongNV;i++)
+        {
+            if (dsnv[i].getHoatDong())
+            {
+                hd++;
+            }
+        }
+        System.out.print("So nhan vien dang hoat dong: "+ hd +" : ");
+        for (int i=0;i<soLuongNV;i++)
+        {
+            if (dsnv[i].getHoatDong())
+            {
+                System.out.print(dsnv[i].getHo()+" "+dsnv[i].getTen()+"  ,  ");
+            }
+        }
+
+        System.out.print("\n");
+        System.out.print("So nhan vien khong hoat dong: "+ (soLuongNV - hd) +" : ");
+        for (int i=0;i<soLuongNV;i++)
+        {
+            if (!dsnv[i].getHoatDong())
+            {
+                System.out.print(dsnv[i].getHo()+" "+dsnv[i].getTen()+"  ,  ");
+            }
+        }
+        System.out.println("\n");
+
+    }
+
+    
+}
---
+++ NhanVien.java
@@ -0,0 +1,227 @@
+
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;;
+public class NhanVien 
+{
+    Scanner input = new Scanner(System.in);
+    private String maNV,ho,ten,chucVu,gioiTinh,SDT;
+    private boolean hoatDong;
+    private LocalDate ngaySinh;
+    private double heSoLuong;
+
+    public NhanVien()
+    {
+        maNV=  null;
+        ho = null;
+        ten = null;
+        gioiTinh= null;
+        ngaySinh = null;
+        SDT= null;
+        hoatDong = false;
+        heSoLuong = 0;
+    }
+    public NhanVien(String maNV,String ho,String ten,String gioiTinh,LocalDate ngaySinh,String chucVu,String SDT,boolean hoatDong,double heSoLuong)
+    {
+        this.maNV = maNV;
+        this.ho=ho;
+        this.ten = ten;
+        this.gioiTinh = gioiTinh;
+        this.ngaySinh = ngaySinh;
+        this.chucVu = chucVu;
+        this.SDT = SDT;
+        this.hoatDong = hoatDong;
+        this.heSoLuong = heSoLuong;
+        
+    }
+    public NhanVien(NhanVien nv)
+    {
+        maNV=nv.maNV;
+        ho=nv.ho;
+        ten=nv.ten;
+        gioiTinh=nv.gioiTinh;
+        ngaySinh=nv.ngaySinh;
+        chucVu=nv.chucVu;
+        SDT= nv.SDT;
+        hoatDong = nv.hoatDong;
+        heSoLuong= nv.heSoLuong;
+    }
+    public void nhap()
+    {
+        System.out.print(" - Ma Nv: ");
+        maNV = input.nextLine();
+        System.out.print(" - Ho: ");
+        ho = input.nextLine();
+        System.out.print(" - Ten: ");
+        ten = input.nextLine();
+        System.out.print(" - Gioi tinh(Nam/Nu)?:  ");
+        gioiTinh = input.nextLine();
+        System.out.print(" - Ngay sinh(yyyy-mm-dd): ");
+        String ngaySinhTemp = input.nextLine();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        ngaySinh = LocalDate.parse(ngaySinhTemp, formatter);
+        System.out.print(" - Chuc vu: ");
+        chucVu = input.nextLine();
+        System.out.print(" - He so luong: ");
+        heSoLuong = input.nextDouble();
+        input.nextLine();
+        System.out.print(" - SDT: ");
+        SDT = input.nextLine();
+        System.out.print(" - Hoat dong(True/False)?: ");
+        hoatDong = input.nextBoolean();     
+    }
+    public void xuat()
+    {
+        System.out.print(maNV+"         "+ho+" "+ten+"         "+gioiTinh+"         "+ngaySinh+"         "+chucVu+"         "+heSoLuong+"         "+SDT+"         "+hoatDong);
+    }
+
+//GET
+    public String getMaNV()
+    {
+        return maNV;
+    }
+    public String getHo()
+    {
+        return ho;
+    }
+    public String getTen()
+    {
+        return ten;
+    }
+    public String getGioiTinh()
+    {
+        return gioiTinh;
+    }
+    public LocalDate getNgaySinh()
+    {
+        return ngaySinh;
+    }
+    public String getChucVu()
+    {
+        return chucVu;
+    }
+    public String getSDT()
+    {
+        return SDT;
+    }
+    public boolean getHoatDong()
+    {
+        return hoatDong;
+    }
+    public Double getHeSoLuong()
+    {
+        return heSoLuong;
+    }
+    
+//SET
+    public void setMaNV(String data)
+    {
+        maNV = data;
+    }
+    public void setHo(String data)
+    {
+        ho = data;
+    }
+    public void setTen(String data)
+    {
+        ten = data;
+    }
+        public void setGioiTinh(String data)
+    {
+        gioiTinh = data;
+    }
+    public void setNgaySinh(LocalDate data)
+    {
+        ngaySinh = data;
+    }
+    public void setChucVu(String data)
+    {
+        chucVu = data;
+    }
+    public void setSDT(String data)
+    {
+        SDT = data;
+    }
+    public void setHoatDong(boolean data)
+    {
+        hoatDong = data;
+    }
+    public void setHeSoLuong(double data)
+    {
+        heSoLuong = data;
+    }
+    public void sua()
+    {
+        System.out.println("---------------");
+        System.out.println("1. Ma nhan vien");
+        System.out.println("2. Ho");
+        System.out.println("3. Ten");
+        System.out.println("4. Gioi tinh");
+        System.out.println("5. ngay sinh");
+        System.out.println("6. Chuc vu");
+        System.out.println("7. He so luong");
+        System.out.println("8. SDT");
+        System.out.println("9. Hoat dong\n");
+        System.out.print("CHon thong tin can sua: ");
+        int n = input.nextInt();
+        input.nextLine();
+        System.out.print("CHon noi dung can sua: ");
+        String nd = input.nextLine();
+        
+        switch(n)
+        {
+            case 1:
+            {
+                setMaNV(nd);
+            } break;
+
+            case 2:
+            {
+                setHo(nd);
+            } break;
+            
+            case 3:
+            {
+                setTen(nd);
+            } break;
+            
+            case 4:
+            {
+                setGioiTinh(nd);
+            } break;
+            
+            case 5:
+            {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                LocalDate ns = LocalDate.parse(nd, formatter);
+                setNgaySinh(ns);
+            } break;
+            
+            case 6:
+            {
+                setChucVu(nd);
+            } break;
+
+            case 7:
+            {
+                setHeSoLuong(Double.parseDouble(nd));
+            } break;
+            case 8:
+            {
+                setSDT(nd);
+            }
+            case 9:
+            {
+                boolean bl;
+                bl = Boolean.parseBoolean(nd);
+                setHoatDong(bl);
+            }
+            default:
+            {
+                System.out.print("Sai!");
+            }
+        }
+
+    }
+
+}
