387574de6d45fbab2c0e81b13c26d3c59ffec72a
GLstarsky <147454401+GLstarsky@users.noreply.github.com>
2023-12-17 01:47:36 +0700

    Update QuanLyDSBN.java

--- QuanLyDSBN.java
+++ QuanLyDSBN.java
@@ -4,11 +4,12 @@ public class QuanLyDSBN {
     private DSBN dsbn;
 
     public void Menu() {
-        System.out.print("----------------");
         dsbn = new DSBN();
         int menu = 0;
+        dsbn.docData();
         Scanner sc = new Scanner(System.in);
         while (menu != 8) {
+            System.out.print("\n----------------");
             System.out.print("\n1) Nhap DSBN." + "\n2) Xem DSBN." + "\n3) Them BN." + "\n4) Xoa BN." + "\n5) Sua thong tin BN." + "\n6) Tim kiem." + "\n7) Thong ke. " + "\n8) Ket thuc.");
             System.out.print("\nHay chon chuc nang: ");
             menu = sc.nextInt();
@@ -25,25 +26,18 @@ public class QuanLyDSBN {
                     break;
                 case 4:
                     System.out.print("\nNhap ma benh nhan muon xoa: ");
-                    String ma1 = sc.nextLine();
+                    int ma1 = sc.nextInt();
                     dsbn.XoaBN(ma1);
                     break;
                 case 5:
                     dsbn.SuaTTBN();
                     break;
                 case 6:
-                    System.out.print("\nNhap ma benh nhan can tim: ");
-                    String ma2 = sc.nextLine();
-                    int foundIndex = dsbn.TimkiemBN(ma2);
-                    if (foundIndex != -1) {
-                        System.out.println("Benh nhan da duoc tim thay o vi tri " + foundIndex);
-                    } else {
-                        System.out.println("Khong tim thay thong tin benh nhan.");
-                    }
+                    dsbn.TimkiemBN();
                     break;
-                //case 7:
-                //dsbn.ThongKe();
-                //break;
+                case 7:
+                dsbn.menuThongKe();
+                break;
             }
         }
     }
