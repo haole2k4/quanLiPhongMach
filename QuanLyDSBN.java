f579296a59473d1137342dc02c2da46b7b9bfdf3
GLstarsky <147454401+GLstarsky@users.noreply.github.com>
2023-12-17 21:50:30 +0700

    Update QuanLyDSBN.java

--- QuanLyDSBN.java
+++ QuanLyDSBN.java
@@ -1,44 +1,12 @@
-import java.util.Scanner;
-
 public class QuanLyDSBN {
-    private DSBN dsbn;
-
-    public void Menu() {
-        dsbn = new DSBN();
-        int menu = 0;
-        dsbn.docData();
-        Scanner sc = new Scanner(System.in);
-        while (menu != 8) {
-            System.out.print("\n----------------");
-            System.out.print("\n1) Nhap DSBN." + "\n2) Xem DSBN." + "\n3) Them BN." + "\n4) Xoa BN." + "\n5) Sua thong tin BN." + "\n6) Tim kiem." + "\n7) Thong ke. " + "\n8) Ket thuc.");
-            System.out.print("\nHay chon chuc nang: ");
-            menu = sc.nextInt();
-            sc.nextLine();
-            switch (menu) {
-                case 1:
-                    dsbn.NhapBN();
-                    break;
-                case 2:
-                    dsbn.XuatBN();
-                    break;
-                case 3:
-                    dsbn.ThemBN();
-                    break;
-                case 4:
-                    System.out.print("\nNhap ma benh nhan muon xoa: ");
-                    int ma1 = sc.nextInt();
-                    dsbn.XoaBN(ma1);
-                    break;
-                case 5:
-                    dsbn.SuaTTBN();
-                    break;
-                case 6:
-                    dsbn.TimkiemBN();
-                    break;
-                case 7:
-                dsbn.menuThongKe();
-                break;
-            }
+    public void thaoTac(){
+        try {
+            DSBN qlDSBN = new DSBN();
+            qlDSBN.docData("data/BenhNhan.txt");
+            qlDSBN.menuBenhNhan();
+            qlDSBN.ghiFile("data/BenhNhan.txt");
+        } catch (Exception e){
+            System.out.println("\nLoi khong the doc file!\n");
         }
     }
 }
