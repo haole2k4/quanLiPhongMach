ac045a9e4a13d2a068a0a4b5a8fc8412d5bce83b
haole2k4 <ahihihaodepzai@gmail.com>
2023-12-18 03:40:12 +0700

    BẮT BUỘC THEO FORM NÀY!!!!!!

---
+++ interfaceDanhSach.java
@@ -0,0 +1,11 @@
+
+
+public interface interfaceDanhSach {
+    public void Them(int soluong);
+    public void Xoa(String maCanXoa);
+    public void Sua(String maCanSua);
+    public void docData(String filePath);
+    public void ghiData(String filepath);
+    public void menuQuanLi();
+    
+} 
